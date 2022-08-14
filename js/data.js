26b24b0631f40fdab8b7e4d4ad1b98a7225bfe83
Durgeshkt <98807002+Durgeshkt@users.noreply.github.com>
2022-08-15 01:59:08 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,27 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Durgesh Kumar Tiwari'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Durgesh Kumar Tiwari'},
+    {name: 'Email', value: 'durgeshkumartiwari007@gmail.com'},
+    {name: 'Country', value: 'india'},
+    {name: 'City', value: 'Bihar'},
+  ],
+  github: [
+    {name: 'GitHub', value: 'Durgeshkt'},
+  ],
+  hacker: [
+
+    {name: 'hackthebox', value: 'https://www.hackthebox.com/profile/69432' },
+    {name: 'tryhackme', value: 'https://tryhackme.com/p/Godzilla'},
+  ],
+  resume: 'Durgesh.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://linkedin/in/dkt7'},
+    {name: 'GitHub', value: 'https://github.com/Durgeshkt'},
+    {name: 'Instagram', value: 'https://instagram/panditdurgesh007'},
+  
+  ]
+}
+
